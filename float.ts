c52b12154a41e7f5c2e07301c7d8a2f34810a374
Jack Lee <jack20499@gmail.com>
2022-11-01 01:23:57 +0800

    feat: add float.ts

---
+++ float.ts
@@ -0,0 +1,15 @@
+
+function compareFloatNumber(a: number, b: number) {
+    if (a === b) {
+        console.log(true);
+    } else {
+        console.log(false)
+    }
+}
+
+let aa: number = 0.3;
+let bb: number = 0.1 * 3.0;
+compareFloatNumber(aa, bb);
+
+
+
