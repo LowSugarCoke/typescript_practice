0ff8474eba5c654816ed0990a10be3690296fb4f
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-11-01 17:14:30 +0800

    feat($basic): add literal in basic/literal.ts
    *Ref: https://ithelp.ithome.com.tw/articles/10215270

---
+++ 01-basic/literal.js
@@ -0,0 +1,15 @@
+"use strict";
+// let addOp = function (n1: number, n2: number) {
+//     return n1 + n2;
+// }
+let powerOp = function (n1, n2) {
+    return n1 ** 2;
+};
+let powerOpWithNoParamsAnnotation = function (n1, n2) {
+    return n1 ** n2;
+};
+let infoAboutMaxwell = {
+    name: 'Maxwell',
+    age: 20,
+    hasPet: false,
+};
---
+++ 01-basic/literal.ts
@@ -0,0 +1,38 @@
+
+
+// let addOp = function (n1: number, n2: number) {
+//     return n1 + n2;
+// }
+
+// let subtractOp = function (n1: number, n2: number) {
+//     return n1 - n2;
+// }
+
+// let MultiplyOp = function (n1: number, n2: number) {
+//     return n1 * n2;
+// }
+
+// let divideOp = function (n1: number, n2: number) {
+//     return n1 / n2;
+// }
+
+type MathOperator = (n1: number, n2: number) => number;
+let powerOp: MathOperator = function (n1: number, n2: number): number {
+    return n1 ** 2;
+};
+
+let powerOpWithNoParamsAnnotation: MathOperator = function (n1, n2) {
+    return n1 ** n2;
+};
+
+type PersonInfo = {
+    name: string,
+    age: number,
+    hasPet: boolean
+};
+
+let infoAboutMaxwell: PersonInfo = {
+    name: 'Maxwell',
+    age: 20,
+    hasPet: false,
+};
