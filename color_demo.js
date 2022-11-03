0bce3c52f6ac21067fbc04745fb15952602b9d91
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-11-03 14:11:22 +0800

    feat(color): add color in color_demo.ts
    *Ref: https://www.youtube.com/watch?v=PEy2zilkBf4&ab_channel=%E5%81%B7%E7%B1%B3%E9%A8%8E%E5%B7%B4%E5%93%A5
    Time: 1:05:00

---
+++ color_demo.js
@@ -0,0 +1,4 @@
+"use strict";
+// // declare function require(name: string): any;
+// var colors = require("colors");
+// console.log('hello world'.green);
---
+++ color_demo.ts
@@ -0,0 +1,3 @@
+// // declare function require(name: string): any;
+// var colors = require("colors");
+// console.log('hello world'.green);
